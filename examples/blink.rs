191a68bf5f921a3d0c708ec38bc52a4dd3b6727b
Rukai <rubickent@gmail.com>
2021-04-30 23:44:17 +1000

    Implement ModeButton and Led abstraction

--- examples/blink.rs
+++ examples/blink.rs
@@ -1,12 +1,9 @@
 #![no_main]
 #![no_std]
 
-use embedded_hal::digital::v2::InputPin;
-use embedded_hal::digital::v2::OutputPin;
 use nrf52840_hal as hal;
-use nrf52840_hal::gpio::Level;
 use rtt_target::{rprintln, rtt_init_print};
-use particle_argon::board_pins;
+use particle_argon::{ModeButton, Led};
 
 #[panic_handler] // panicking behavior
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -21,15 +18,15 @@ fn main() -> ! {
     let p = hal::pac::Peripherals::take().unwrap();
     let port0 = hal::gpio::p0::Parts::new(p.P0);
     let port1 = hal::gpio::p1::Parts::new(p.P1);
-    let button: board_pins::MD<_> = port0.p0_11.into_pullup_input();
-    let mut led: board_pins::D7<_> = port1.p1_12.into_push_pull_output(Level::Low);
+    let button = ModeButton::new(port0.p0_11);
+    let mut led  = Led::new(port1.p1_12);
 
     rprintln!("Blinky button demo starting");
     loop {
-        if button.is_high().unwrap() {
-            led.set_high().unwrap();
+        if button.is_held_down() {
+            led.on();
         } else {
-            led.set_low().unwrap();
+            led.off();
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,51 @@
 #![no_std]
 
 pub mod board_pins;
+
+use nrf52840_hal::gpio::p0::P0_11;
+use nrf52840_hal::gpio::p1::P1_12;
+use nrf52840_hal::gpio::{Input, Output, PullUp, PushPull, Disconnected, Level};
+use nrf52840_hal::prelude::*;
+
+/// Simple abstraction around the MODE button connected to the MD/P0.11 pin.
+pub struct ModeButton {
+    pin: P0_11<Input<PullUp>>,
+}
+
+impl ModeButton {
+    /// Initialize the button.
+    pub fn new(pin: P0_11<Disconnected>) -> Self {
+        ModeButton {
+            pin: pin.into_pullup_input(),
+        }
+    }
+
+    /// Returns true iff button is physically held down.
+    pub fn is_held_down(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+}
+
+/// Simple abstraction around the user LED at the top right of the board button connected to the D7/P1.12 pin.
+pub struct Led {
+    pin: P1_12<Output<PushPull>>,
+}
+
+impl Led {
+    /// Initialize the LED, LED remains off.
+    pub fn new(pin: P1_12<Disconnected>) -> Self {
+        Led {
+            pin: pin.into_push_pull_output(Level::Low),
+        }
+    }
+
+    /// Turn the LED on.
+    pub fn on(&mut self) {
+        self.pin.set_high().unwrap();
+    }
+
+    /// Turn the LED off.
+    pub fn off(&mut self) {
+        self.pin.set_low().unwrap();
+    }
+}
